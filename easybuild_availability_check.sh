88a04bc0f8089874f011e03754e734c458387280
Sabry Razick <sabry.razick@usit.uio.no>
2019-03-08 10:13:42 +0100

    initial version of easybuild_availability_check

---
+++ easybuild_availability_check.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# find all the module-file locations, get the default versions 
+# (TODO: modify to use when defualt version not available
+# Search easybuild for that version 
+# (TODO; Fix  easybuld should be loaded for this to work at the moment)
+
+for MPATH in $(echo $MODULEPATH | tr : ' '); 
+do 
+	#echo $MPATH 
+	if [ -d  $MPATH ]
+	then
+		for TOOL in $(ls $MPATH | head -n 3); 
+		do 
+			#echo "$MPATH/$TOOL"
+			if [ -f  "$MPATH/$TOOL/.version" ]
+			then
+				printf "eb --search \"$TOOL-" 
+				cat "$MPATH/$TOOL/.version" |grep set |awk -F '\\"' '{print $2 "\""}' 
+				echo ""
+			#else
+			#	echo "$MPATH/$TOOL/.version not found"
+			fi
+		done
+	fi
+done
