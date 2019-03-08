40cb5a45824989a3df094b16302f3eade0fd1f95
Sabry Razick <sabry.razick@usit.uio.no>
2019-03-08 10:18:43 +0100

    easybuild command construct

--- easybuild_availability_check.sh
+++ easybuild_availability_check.sh
@@ -12,11 +12,15 @@ do
 		for TOOL in $(ls $MPATH | head -n 3); 
 		do 
 			#echo "$MPATH/$TOOL"
+			ebcommand="eb "
 			if [ -f  "$MPATH/$TOOL/.version" ]
 			then
-				printf "eb --search \"$TOOL-" 
-				cat "$MPATH/$TOOL/.version" |grep set |awk -F '\\"' '{print $2 "\""}' 
-				echo ""
+				#printf "eb --search \"$TOOL-" 
+				#cat "$MPATH/$TOOL/.version" |grep set |awk -F '\\"' '{print $2 "\""}' 
+
+				ebcommand="$ebcommand --search \"$TOOL-" 
+			        ebcommand="$ebcommand $MPATH/$TOOL/.version" |grep set |awk -F '\\"' '{print $2 "\""}' 
+				echo $ebcommand
 			#else
 			#	echo "$MPATH/$TOOL/.version not found"
 			fi
